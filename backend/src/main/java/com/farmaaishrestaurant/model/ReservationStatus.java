8fbfd0774b9cd56caf79231cdf8447120ebbf8ff
yashdahat <yashdahat16@gmail.com>
2026-08-03 22:51:55 +0530

    clear: reset branch content

--- backend/src/main/java/com/farmaaishrestaurant/model/ReservationStatus.java
+++
@@ -1,9 +0,0 @@
-package com.farmaaishrestaurant.model;
-
-public enum ReservationStatus {
-    PENDING,
-    CONFIRMED,
-    CANCELLED,
-    COMPLETED,
-    NO_SHOW
-}
--- backend/src/main/java/com/farmaaishrestaurant/model/Role.java
+++
@@ -1,6 +0,0 @@
-package com.farmaaishrestaurant.model;
-
-public enum Role {
-    ADMIN,
-    USER
-}
